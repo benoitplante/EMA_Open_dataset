0f4475403217f000205e4ce7e01cfe62d5f0d726
benoit.plante@uqtr.ca <jane@example.com>
2022-05-05 13:52:33 -0400

    Changement dans les liens

--- R/script.R
+++ R/script.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 # Creation des dataframes - EMA2018 / EMA2019 / EMA_total
 
-list_of_files <- list.files(path = "C:/Users/planteb/Desktop/Projet R/blogue/EMA_Open_dataset/data/raw/UT1000_ema/UT1000_fall2018",
+list_of_files <- list.files(path = "C:/Users/planteb/Desktop/Projet R/EMA_Open_dataset/data/raw/UT1000_ema/UT1000_fall2018",
                             recursive = TRUE,
                             pattern = "momentary_emas.csv$",
                             full.names = TRUE)
@@ -10,7 +10,7 @@ list_of_files <- list.files(path = "C:/Users/planteb/Desktop/Projet R/blogue/EMA
 df_EMA_2018 <- readr::read_csv(list_of_files, id = "file_name")
 
 
-list_of_files <- list.files(path = "C:/Users/planteb/Desktop/Projet R/blogue/EMA_Open_dataset/data/raw/UT1000_ema/UT1000_spring2019",
+list_of_files <- list.files(path = "C:/Users/planteb/Desktop/Projet R/EMA_Open_dataset/data/raw/UT1000_ema/UT1000_spring2019",
                             recursive = TRUE,
                             pattern = "momentary_emas.csv$",
                             full.names = TRUE)
@@ -19,7 +19,7 @@ df_EMA_2019 <- readr::read_csv(list_of_files, id = "file_name")
 
 df_EMA_total <- rbind(df_EMA_2018, df_EMA_2019)
 
-df_participant <- read_delim("C:/Users/planteb/Desktop/Projet R/blogue/EMA_Open_dataset/data/raw/participant.txt", delim = ",")
+df_participant <- read_delim("C:/Users/planteb/Desktop/Projet R/EMA_Open_dataset/data/raw/participant.txt", delim = ",")
 df_participant <- rename(df_participant, pid = beiwe_id)
 
 df_participant_EMA <- df_participant %>% inner_join(df_EMA_total, by = "pid")
